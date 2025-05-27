2c344ac9775522ff558dce211cb9aa373e5b2cf7
Youvald <114990773+Youvald@users.noreply.github.com>
2025-05-27 23:18:39 +0300

    vse work bez oplaty ta nazarehanyi ne mozhe zamovlyaty

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,46 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from .models import CustomUser
+from .models import DeliveryAddress
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = DeliveryAddress
+        fields = ['city', 'street', 'building', 'apartment', 'is_default']
+        labels = {
+            'city': 'Місто',
+            'street': 'Вулиця',
+            'building': 'Будинок',
+            'apartment': 'Квартира',
+            'is_default': 'Основна адреса',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['city'].initial = 'Івано-Франківськ'
+        self.fields['city'].widget.attrs['readonly'] = True
 
 class RegisterForm(forms.ModelForm):
-    password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Підтвердження пароля", widget=forms.PasswordInput)
+    password1 = forms.CharField(
+        label="Пароль",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    password2 = forms.CharField(
+        label="Підтвердження пароля",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
 
     class Meta:
         model = CustomUser
         fields = ['first_name', 'phone']
+        labels = {
+            'first_name': 'Імʼя',
+            'phone': 'Телефон',
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
     def clean_password2(self):
         p1 = self.cleaned_data.get('password1')
@@ -28,9 +60,3 @@ class LoginForm(AuthenticationForm):
     username = forms.CharField(label="Номер телефону")
     password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
 
-from .models import DeliveryAddress
-
-class AddressForm(forms.ModelForm):
-    class Meta:
-        model = DeliveryAddress
-        fields = ['city', 'street', 'building', 'apartment', 'is_default']
