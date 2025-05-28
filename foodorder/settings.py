f682b0fffae60532340e579322bfc3b1f87c9822
Youvald <114990773+Youvald@users.noreply.github.com>
2025-05-28 16:53:33 +0300

    #5

--- foodorder/settings.py
+++ foodorder/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -143,3 +143,6 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
